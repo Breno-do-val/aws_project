55d89e12ec7fbeee427b62f0a70a49147434e8b1
breno-do-val <brenorvale@gmail.com>
2020-10-17 17:29:11 -0300

    application concluded

--- src/main/java/br/com/siecola/aws_project01/repository/ProductRepository.java
+++ src/main/java/br/com/siecola/aws_project01/repository/ProductRepository.java
@@ -2,11 +2,9 @@ package br.com.siecola.aws_project01.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
-
 import br.com.siecola.aws_project01.model.Product;
 
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 	
 	Optional<Product> findByCode(String code);
 	
